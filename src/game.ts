4324b74b28d28a6214e0d922dfd321282b5a7f28
Frédéric Léveillée <frederic.leveillee@firstwordgroup.com>
2019-08-02 08:48:26 -0400

    Now displays a background

--- src/game.ts
+++ src/game.ts
@@ -1,11 +1,18 @@
-// import {Canvas, Character, Sprite} from '@spibo-studio/spibo-studio';
-import {Canvas, Character, Sprite} from '../../spibo-studio/src/spibo-studio';
+// import {Background, Canvas, Character, Sprite} from '@spibo-studio/spibo-studio';
+import {Background, Canvas, Character, Sprite} from '../../spibo-studio/src/spibo-studio';
+
 
 document.addEventListener("DOMContentLoaded", function (): void {
 
+
   const canvas = new Canvas(640, 480);
 
+  const forest = new Background('images/backgrounds/bg-forest-2.jpg', 1300, 650);
+
+  canvas.background = forest;
+
   const edgar = new Character("images/characters/edgar.gif", 355, 140, 5);
+  edgar.yPos = 300;
   edgar.sprites.push(new Sprite('movingUp', 95, 10, 80, 120));
   edgar.sprites.push(new Sprite('movingDown', 10, 10, 80, 120));
   edgar.sprites.push(new Sprite('movingLeft', 265, 10, 80, 120));
