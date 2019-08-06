47f9b0a653a59deb7a364fea40847d91e02ff66e
Frédéric Léveillée <frederic.leveillee@firstwordgroup.com>
2019-08-06 09:26:20 -0400

    Now drawing a background!

--- src/game.ts
+++ src/game.ts
@@ -4,11 +4,9 @@ import { Background, Canvas, Character, Sprite } from '../../spibo-studio/src/sp
 document.addEventListener('DOMContentLoaded', function(): void {
   const canvas = new Canvas(640, 480);
 
-  const forest = new Background('images/backgrounds/bg-forest-2.jpg', 1300, 650);
+  canvas.background = new Background('images/backgrounds/bg-forest-2.jpg', 1300, 650, true);
 
-  canvas.background = forest;
-
-  const edgar = new Character('images/characters/edgar.gif', 355, 140, 5);
+  const edgar = new Character('images/characters/edgar.gif', 355, 140, 50);
   edgar.yPos = 300;
   edgar.sprites.push(new Sprite('movingUp', 95, 10, 80, 120));
   edgar.sprites.push(new Sprite('movingDown', 10, 10, 80, 120));
